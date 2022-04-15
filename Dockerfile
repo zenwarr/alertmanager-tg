6f41270627f658f9352770ce8d05b2ac29a92e1e
zenwarr <zenw@yandex.ru>
2022-04-15 18:11:23 +0600

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM denoland/deno:alpine-1.20.6 as builder
+
+ADD ./src /src
+WORKDIR /src
+
+RUN cd /src && deno bundle start.ts -- /tgbot.js
+
+FROM denoland/deno:alpine-1.20.6
+
+USER deno
+
+COPY --from=builder /tgbot.js /tgbot.js
+
+CMD ["run", "--allow-net", "--allow-env", "/tgbot.js"]
---
+++ bundle.js
@@ -0,0 +1,256 @@
+// deno-fmt-ignore-file
+// deno-lint-ignore-file
+// This code was bundled using `deno bundle` and it's not recommended to edit it manually
+
+function delay(ms, options = {}) {
+    const { signal  } = options;
+    if (signal?.aborted) {
+        return Promise.reject(new DOMException("Delay was aborted.", "AbortError"));
+    }
+    return new Promise((resolve, reject)=>{
+        const abort = ()=>{
+            clearTimeout(i);
+            reject(new DOMException("Delay was aborted.", "AbortError"));
+        };
+        const done = ()=>{
+            signal?.removeEventListener("abort", abort);
+            resolve();
+        };
+        const i = setTimeout(done, ms);
+        signal?.addEventListener("abort", abort, {
+            once: true
+        });
+    });
+}
+const ERROR_SERVER_CLOSED = "Server closed";
+const INITIAL_ACCEPT_BACKOFF_DELAY = 5;
+const MAX_ACCEPT_BACKOFF_DELAY = 1000;
+class Server {
+    #port;
+    #host;
+    #handler;
+    #closed = false;
+    #listeners = new Set();
+    #httpConnections = new Set();
+    #onError;
+    constructor(serverInit){
+        this.#port = serverInit.port;
+        this.#host = serverInit.hostname;
+        this.#handler = serverInit.handler;
+        this.#onError = serverInit.onError ?? function(error) {
+            console.error(error);
+            return new Response("Internal Server Error", {
+                status: 500
+            });
+        };
+    }
+    async serve(listener) {
+        if (this.#closed) {
+            throw new Deno.errors.Http(ERROR_SERVER_CLOSED);
+        }
+        this.#trackListener(listener);
+        try {
+            return await this.#accept(listener);
+        } finally{
+            this.#untrackListener(listener);
+            try {
+                listener.close();
+            } catch  {}
+        }
+    }
+    async listenAndServe() {
+        if (this.#closed) {
+            throw new Deno.errors.Http(ERROR_SERVER_CLOSED);
+        }
+        const listener = Deno.listen({
+            port: this.#port ?? 80,
+            hostname: this.#host ?? "0.0.0.0",
+            transport: "tcp"
+        });
+        return await this.serve(listener);
+    }
+    async listenAndServeTls(certFile, keyFile) {
+        if (this.#closed) {
+            throw new Deno.errors.Http(ERROR_SERVER_CLOSED);
+        }
+        const listener = Deno.listenTls({
+            port: this.#port ?? 443,
+            hostname: this.#host ?? "0.0.0.0",
+            certFile,
+            keyFile,
+            transport: "tcp"
+        });
+        return await this.serve(listener);
+    }
+    close() {
+        if (this.#closed) {
+            throw new Deno.errors.Http(ERROR_SERVER_CLOSED);
+        }
+        this.#closed = true;
+        for (const listener of this.#listeners){
+            try {
+                listener.close();
+            } catch  {}
+        }
+        this.#listeners.clear();
+        for (const httpConn of this.#httpConnections){
+            this.#closeHttpConn(httpConn);
+        }
+        this.#httpConnections.clear();
+    }
+    get closed() {
+        return this.#closed;
+    }
+    get addrs() {
+        return Array.from(this.#listeners).map((listener)=>listener.addr
+        );
+    }
+    async #respond(requestEvent, httpConn, connInfo) {
+        let response;
+        try {
+            response = await this.#handler(requestEvent.request, connInfo);
+        } catch (error) {
+            response = await this.#onError(error);
+        }
+        try {
+            await requestEvent.respondWith(response);
+        } catch  {
+            return this.#closeHttpConn(httpConn);
+        }
+    }
+    async #serveHttp(httpConn1, connInfo1) {
+        while(!this.#closed){
+            let requestEvent;
+            try {
+                requestEvent = await httpConn1.nextRequest();
+            } catch  {
+                break;
+            }
+            if (requestEvent === null) {
+                break;
+            }
+            this.#respond(requestEvent, httpConn1, connInfo1);
+        }
+        this.#closeHttpConn(httpConn1);
+    }
+    async #accept(listener) {
+        let acceptBackoffDelay;
+        while(!this.#closed){
+            let conn;
+            try {
+                conn = await listener.accept();
+            } catch (error) {
+                if (error instanceof Deno.errors.BadResource || error instanceof Deno.errors.InvalidData || error instanceof Deno.errors.UnexpectedEof || error instanceof Deno.errors.ConnectionReset || error instanceof Deno.errors.NotConnected) {
+                    if (!acceptBackoffDelay) {
+                        acceptBackoffDelay = INITIAL_ACCEPT_BACKOFF_DELAY;
+                    } else {
+                        acceptBackoffDelay *= 2;
+                    }
+                    if (acceptBackoffDelay >= 1000) {
+                        acceptBackoffDelay = MAX_ACCEPT_BACKOFF_DELAY;
+                    }
+                    await delay(acceptBackoffDelay);
+                    continue;
+                }
+                throw error;
+            }
+            acceptBackoffDelay = undefined;
+            let httpConn;
+            try {
+                httpConn = Deno.serveHttp(conn);
+            } catch  {
+                continue;
+            }
+            this.#trackHttpConnection(httpConn);
+            const connInfo = {
+                localAddr: conn.localAddr,
+                remoteAddr: conn.remoteAddr
+            };
+            this.#serveHttp(httpConn, connInfo);
+        }
+    }
+     #closeHttpConn(httpConn2) {
+        this.#untrackHttpConnection(httpConn2);
+        try {
+            httpConn2.close();
+        } catch  {}
+    }
+     #trackListener(listener1) {
+        this.#listeners.add(listener1);
+    }
+     #untrackListener(listener2) {
+        this.#listeners.delete(listener2);
+    }
+     #trackHttpConnection(httpConn3) {
+        this.#httpConnections.add(httpConn3);
+    }
+     #untrackHttpConnection(httpConn4) {
+        this.#httpConnections.delete(httpConn4);
+    }
+}
+async function serve(handler1, options = {}) {
+    const server = new Server({
+        port: options.port ?? 8000,
+        hostname: options.hostname ?? "0.0.0.0",
+        handler: handler1,
+        onError: options.onError
+    });
+    if (options?.signal) {
+        options.signal.onabort = ()=>server.close()
+        ;
+    }
+    return await server.listenAndServe();
+}
+const BOT_TOKEN = Deno.env.get("BOT_TOKEN");
+const CHAT_ID = Deno.env.get("CHAT_ID");
+if (!BOT_TOKEN) {
+    console.error("BOT_TOKEN not provided");
+    Deno.exit(1);
+}
+if (!CHAT_ID) {
+    console.error("CHAT_ID not provided");
+    Deno.exit(1);
+}
+async function telegramSend(message) {
+    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            chat_id: CHAT_ID,
+            text: message
+        })
+    });
+}
+function handleWebhook(data) {
+    return telegramSend(formatMsg(data));
+}
+function formatMsg(data) {
+    return "test_msg";
+}
+const HTTP_PORT = +(Deno.env.get("HTTP_PORT") || 8080);
+if (isNaN(HTTP_PORT)) {
+    throw new Error("HTTP_PORT must be provided and be a number, got: " + (HTTP_PORT ?? "<not defined>"));
+}
+console.log(`Webhook server is listening on port ${HTTP_PORT}`);
+await serve(handler, {
+    port: HTTP_PORT
+});
+async function handler(req) {
+    const path = new URL(req.url).pathname;
+    if (path === "/" && req.method === "POST") {
+        const contentBuf = await req.arrayBuffer();
+        const content = new TextDecoder().decode(contentBuf);
+        await handleWebhook(JSON.parse(content));
+        return new Response(null, {
+            status: 200
+        });
+    } else {
+        return new Response(null, {
+            status: 404
+        });
+    }
+}
+
